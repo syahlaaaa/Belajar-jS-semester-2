90cbce33ab4fbe4eccb10d54a38a7b559213ced1
syahlaaaa <dliyasyahla789@gmail.com>
2024-01-16 15:23:15 +0800

    belajar js

---
+++ praktek3/script.js
@@ -0,0 +1,6 @@
+const Negara = "Seychelles";
+const benua = "Afrika"
+const populasi = "99.258"   ;
+var bahasa = "perancis"      ;
+
+console.log(`Negara ${Negara} adalah negara yang berada di benua ${benua} , dengan populasi sebanyak ${populasi} juta jiwa,bahasa nasional ${bahasa}`);
